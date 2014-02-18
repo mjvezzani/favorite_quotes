f0f484b3a460341dceb56c4ea69313c99decfdd6
Michael Vezzani <mjvezzani@gmail.com>
2014-02-18 15:22:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FavoriteQuotes::Application.config.secret_key_base = '9f9ef2ed30b0ba39d3c89b20b1092a288ca899ee4585830043d759e33a18826c686040245e629a9829d564d9c0a626c2de618de68ad712d38f4b80175bbbc857'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FavoriteQuotes::Application.config.session_store :cookie_store, key: '_favorite_quotes_session'
