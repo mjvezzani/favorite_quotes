489d808d0251437de0b2dd80c238a678ab3d4062
Michael Vezzani <mjvezzani@gmail.com>
2014-02-19 16:08:10 -0500

    Modify User specs

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe User do
 
-  it "is invalid without a user name" do
-    expect(User.create(name: "", password: "foobar", password_confirmation: "foobar")).to be_invalid
-  end
-  it "is invalid without a password" do
-    expect(User.create(name: "Mike Vezzani", password: "", password_confirmation: "foobar")).to be_invalid
-  end
-  it "is invalid without a password_confirmation" do
-    expect(User.create(name: "Mike Vezzani", password: "foobar", password_confirmation: "")).to be_invalid
-  end
-  it "is valid with all attributes present" do
-    expect(User.create(name: "Mike Vezzani", password: "foobar", password_confirmation: "foobar")).to be_valid
+  describe "it has valid fields" do
+    it "is invalid without a user name" do
+      expect(User.create(name: "", password: "foobar", password_confirmation: "foobar")).to be_invalid
+    end
+    it "is invalid without a password" do
+      expect(User.create(name: "Mike Vezzani", password: "", password_confirmation: "foobar")).to be_invalid
+    end
+    it "is invalid without a password_confirmation" do
+      expect(User.create(name: "Mike Vezzani", password: "foobar", password_confirmation: "")).to be_invalid
+    end
+    it "is valid with all attributes present" do
+      expect(User.create(name: "Mike Vezzani", password: "foobar", password_confirmation: "foobar")).to be_valid
+    end
   end
 
   describe "new user has a unique username" do
@@ -23,5 +25,4 @@ describe User do
       expect(User.create(name: "Mike Vezzani", password: "jonezez", password_confirmation: "jonezez")).to be_invalid
     end
   end
-
 end
